57d880ee268bd5f509797002b73612944e9863b2
Thomas Paradis <tom.paradis@orange.fr>
2025-08-28 11:23:24 +0200

    ajout blogs

--- client/src/pages/Blog/Blog.jsx
+++ client/src/pages/Blog/Blog.jsx
@@ -1,3 +1,29 @@
+import { useEffect, useState } from "react";
+import Card from "../../components/Common/Card";
+
 export default function Blog() {
-  return <div>Blog</div>;
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    const getAllBlogs = async () => {
+      try {
+        const response = await fetch("http://localhost:2003/blog");
+        const blogsFromBackend = await response.json();
+        console.log(blogsFromBackend);
+        setBlogs(blogsFromBackend);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getAllBlogs();
+  }, []);
+  return (
+    <div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6 mb-4">
+        {blogs.map((blog) => (
+          <Card key={blog._id} blog={blog} />
+        ))}
+      </div>
+    </div>
+  );
 }
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,8 +1,13 @@
 import express from "express";
+
 import userRoutes from "./user.routes.js";
+import blogRoutes from "./blog.routes.js";
 
 const router = express.Router();
 
+router.use("/blog", blogRoutes);
 router.use("/user", userRoutes);
 
 export default router;
+
+// http://localhost:5000
